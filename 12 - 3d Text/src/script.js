176c4200f48fbc1fe88e321ce7490af6ab562e4e
Daumantas <daumis05@gmail.com>
2025-01-19 16:41:32 +0200

    lessons 12 - use easier way to center text, optimize code and add donuts!

--- 12 - 3d Text/src/script.js
+++ 12 - 3d Text/src/script.js
@@ -29,6 +29,7 @@ matcapTexture.colorSpace = THREE.SRGBColorSpace;
 /*
 * Fonts
 */
+const material = new THREE.MeshMatcapMaterial({matcap: matcapTexture})
 
 const fontLoader = new FontLoader();
 fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
@@ -45,13 +46,32 @@ fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
     })
 
     // Center text within the mesh
-    textGeometry.computeBoundingBox()
-    textGeometry.translate(- textGeometry.boundingBox.max.x * 0.5, - textGeometry.boundingBox.max.y * 0.5, - textGeometry.boundingBox.max.z * 0.5);
+    textGeometry.center();
 
-    const textMaterial = new THREE.MeshMatcapMaterial({matcap: matcapTexture})
-    const text = new THREE.Mesh(textGeometry, textMaterial)
+    const text = new THREE.Mesh(textGeometry, material)
     scene.add(text);
 })
+
+/*
+* Objects
+*/
+
+for(let i = 0; i < 100; i++)
+{
+    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+    const donut = new THREE.Mesh(donutGeometry, material)
+    donut.position.x = (Math.random() - 0.5) * 10
+    donut.position.y = (Math.random() - 0.5) * 10
+    donut.position.z = (Math.random() - 0.5) * 10
+
+    donut.rotation.x = Math.random() * Math.PI
+    donut.rotation.y = Math.random() * Math.PI
+
+    const scale = Math.random()
+    donut.scale.set(scale, scale, scale)
+    scene.add(donut)
+
+}
 
 /**
  * Sizes
