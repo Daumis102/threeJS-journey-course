d76e87fa587cc1cb662fc9620b58d7b4b85cdadf
Daumantas <daumis05@gmail.com>
2025-01-26 00:04:38 +0200

    lecture 16 - haunted house - optimize texture images

--- 16 - haunted house/src/script.js
+++ 16 - haunted house/src/script.js
@@ -21,12 +21,12 @@ const scene = new three.Scene()
 * */
 
 const textureLoader = new three.TextureLoader()
-const floorAlphaTexture = textureLoader.load("floor/alpha.jpg")
-const floorColorTexture = textureLoader.load("floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_diff_1k.jpg")
+const floorAlphaTexture = textureLoader.load("floor/alpha.webp")
+const floorColorTexture = textureLoader.load("floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_diff_1k.webp")
 floorColorTexture.colorSpace = three.SRGBColorSpace
-const floorARMTexture = textureLoader.load("floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_arm_1k.jpg")
-const floorNormalTexture = textureLoader.load("floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_nor_gl_1k.jpg")
-const floorDisplacementTexture = textureLoader.load("floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_disp_1k.jpg")
+const floorARMTexture = textureLoader.load("floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_arm_1k.webp")
+const floorNormalTexture = textureLoader.load("floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_nor_gl_1k.webp")
+const floorDisplacementTexture = textureLoader.load("floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_disp_1k.webp")
 floorColorTexture.repeat.set(8, 8)
 floorColorTexture.wrapS = three.RepeatWrapping
 floorColorTexture.wrapT = three.RepeatWrapping
@@ -43,10 +43,10 @@ floorDisplacementTexture.repeat.set(8, 8)
 floorDisplacementTexture.wrapS = three.RepeatWrapping
 floorDisplacementTexture.wrapT = three.RepeatWrapping
 
-const wallColorTexture = textureLoader.load("wall/castle_brick_broken_06_1k/castle_brick_broken_06_diff_1k.jpg")
+const wallColorTexture = textureLoader.load("wall/castle_brick_broken_06_1k/castle_brick_broken_06_diff_1k.webp")
 wallColorTexture.colorSpace = three.SRGBColorSpace
-const wallARMTexture = textureLoader.load("wall/castle_brick_broken_06_1k/castle_brick_broken_06_arm_1k.jpg")
-const wallNormalTexture = textureLoader.load("wall/castle_brick_broken_06_1k/castle_brick_broken_06_nor_gl_1k.jpg")
+const wallARMTexture = textureLoader.load("wall/castle_brick_broken_06_1k/castle_brick_broken_06_arm_1k.webp")
+const wallNormalTexture = textureLoader.load("wall/castle_brick_broken_06_1k/castle_brick_broken_06_nor_gl_1k.webp")
 
 wallColorTexture.wrapS = three.RepeatWrapping
 wallColorTexture.wrapT = three.RepeatWrapping
@@ -57,10 +57,10 @@ wallARMTexture.wrapT = three.RepeatWrapping
 wallNormalTexture.wrapS = three.RepeatWrapping
 wallNormalTexture.wrapT = three.RepeatWrapping
 
-const roofColorTexture = textureLoader.load("roof/roof_slates_02_1k/roof_slates_02_diff_1k.jpg")
+const roofColorTexture = textureLoader.load("roof/roof_slates_02_1k/roof_slates_02_diff_1k.webp")
 roofColorTexture.colorSpace = three.SRGBColorSpace
-const roofARMTexture = textureLoader.load("roof/roof_slates_02_1k/roof_slates_02_arm_1k.jpg")
-const roofNormalTexture = textureLoader.load("roof/roof_slates_02_1k/roof_slates_02_nor_gl_1k.jpg")
+const roofARMTexture = textureLoader.load("roof/roof_slates_02_1k/roof_slates_02_arm_1k.webp")
+const roofNormalTexture = textureLoader.load("roof/roof_slates_02_1k/roof_slates_02_nor_gl_1k.webp")
 
 roofColorTexture.repeat.set(3, 1)
 roofARMTexture.repeat.set(3, 1)
@@ -70,10 +70,10 @@ roofColorTexture.wrapS = three.RepeatWrapping
 roofARMTexture.wrapS = three.RepeatWrapping
 roofNormalTexture.wrapS = three.RepeatWrapping
 
-const bushColorTexture = textureLoader.load("bush/leaves_forest_ground/leaves_forest_ground_diff_1k.jpg")
+const bushColorTexture = textureLoader.load("bush/leaves_forest_ground/leaves_forest_ground_diff_1k.webp")
 bushColorTexture.colorSpace = three.SRGBColorSpace
-const bushARMTexture = textureLoader.load("bush/leaves_forest_ground/leaves_forest_ground_arm_1k.jpg")
-const bushNormalTexture = textureLoader.load("bush/leaves_forest_ground/leaves_forest_ground_nor_gl_1k.jpg")
+const bushARMTexture = textureLoader.load("bush/leaves_forest_ground/leaves_forest_ground_arm_1k.webp")
+const bushNormalTexture = textureLoader.load("bush/leaves_forest_ground/leaves_forest_ground_nor_gl_1k.webp")
 bushColorTexture.repeat.set(2, 1)
 bushARMTexture.repeat.set(2, 1)
 bushNormalTexture.repeat.set(2, 1)
@@ -82,24 +82,24 @@ bushColorTexture.srapS = three.RepeatWrapping
 bushARMTexture.srapS = three.RepeatWrapping
 bushNormalTexture.srapS = three.RepeatWrapping
 
-const graveColorTexture = textureLoader.load("grave/plastered_stone_wall_1k/plastered_stone_wall_diff_1k.jpg")
+const graveColorTexture = textureLoader.load("grave/plastered_stone_wall_1k/plastered_stone_wall_diff_1k.webp")
 graveColorTexture.colorSpace = three.SRGBColorSpace
-const graveARMTexture = textureLoader.load("grave/plastered_stone_wall_1k/plastered_stone_wall_arm_1k.jpg")
-const graveNormalTexture = textureLoader.load("grave/plastered_stone_wall_1k/plastered_stone_wall_nor_gl_1k.jpg")
+const graveARMTexture = textureLoader.load("grave/plastered_stone_wall_1k/plastered_stone_wall_arm_1k.webp")
+const graveNormalTexture = textureLoader.load("grave/plastered_stone_wall_1k/plastered_stone_wall_nor_gl_1k.webp")
 
 graveColorTexture.repeat.set(0.3, 0.4);
 graveARMTexture.repeat.set(0.3, 0.4);
 graveNormalTexture.repeat.set(0.3, 0.4);
 
 
-const doorColorTexture = textureLoader.load("door/color.jpg")
+const doorColorTexture = textureLoader.load("door/color.webp")
 doorColorTexture.colorSpace = three.SRGBColorSpace
-const doorAlphaTexture = textureLoader.load("door/alpha.jpg")
-const doorAmbientOcclusionTexture = textureLoader.load("door/ambientOcclusion.jpg")
-const doorHeightTexture = textureLoader.load("door/height.jpg")
-const doorNormalTexture = textureLoader.load("door/normal.jpg")
-const doorMetalnessTexture = textureLoader.load("door/metalness.jpg")
-const doorRoughnessTexture = textureLoader.load("door/roughness.jpg")
+const doorAlphaTexture = textureLoader.load("door/alpha.webp")
+const doorAmbientOcclusionTexture = textureLoader.load("door/ambientOcclusion.webp")
+const doorHeightTexture = textureLoader.load("door/height.webp")
+const doorNormalTexture = textureLoader.load("door/normal.webp")
+const doorMetalnessTexture = textureLoader.load("door/metalness.webp")
+const doorRoughnessTexture = textureLoader.load("door/roughness.webp")
 
 /**
  * House
