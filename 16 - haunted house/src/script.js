609165d48041cb27e1738ea29b51092accd459c4
Daumantas <daumis05@gmail.com>
2025-01-25 20:24:50 +0200

    lecture 16 - haunted house - some additional setup

--- 16 - haunted house/src/script.js
+++ 16 - haunted house/src/script.js
@@ -1,7 +1,7 @@
-import * as THREE from 'three'
+import * as three from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { Timer } from 'three/addons/misc/Timer.js'
 import GUI from 'lil-gui'
+import {Timer} from "three/addons";
 
 /**
  * Base
@@ -13,27 +13,30 @@ const gui = new GUI()
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new three.Scene()
 
 /**
  * House
  */
 // Temporary sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+const sphere = new three.Mesh(
+    new three.SphereGeometry(1, 32, 32),
+    new three.MeshStandardMaterial({ roughness: 0.7 })
 )
 scene.add(sphere)
+
+// floor
+const floot= THRE
 
 /**
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+const ambientLight = new three.AmbientLight('#ffffff', 0.5)
 scene.add(ambientLight)
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
+const directionalLight = new three.DirectionalLight('#ffffff', 1.5)
 directionalLight.position.set(3, 2, -8)
 scene.add(directionalLight)
 
@@ -64,7 +67,7 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+const camera = new three.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 4
 camera.position.y = 2
 camera.position.z = 5
@@ -77,7 +80,7 @@ controls.enableDamping = true
 /**
  * Renderer
  */
-const renderer = new THREE.WebGLRenderer({
+const renderer = new three.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
