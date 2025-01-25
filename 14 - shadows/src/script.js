17f10888b0504011b9280778b194c4388e4ab430
Daumantas <daumis05@gmail.com>
2025-01-25 19:53:09 +0200

    lessons 14 - shadows - directional light with THREE.VSMShadowMap shadow algorithm and blur

--- 14 - shadows/src/script.js
+++ 14 - shadows/src/script.js
@@ -148,7 +148,7 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.shadowMap.type = THREE.VSMShadowMap
 /**
  * Animate
  */
